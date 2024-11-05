272efc4578c4bbd9226325b67eba88ec824b27d3
Martha Huerta <marthah.112225@gmail.com>
2024-11-05 17:02:01 -0600

    excercise 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,23 @@
-const baseEndpoint = 'https://api.github.com';
+const baseEndpoint = `https://api.github.com`;
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
+// Eliminar location y sus usos
+//Cambiar comillas simples por backticks
 
-function displayUser(username) {
+async function displayUser(username) {
+  // Agregé el try catch y convertí la función en asíncrona
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  try {
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    console.log(typeof response);
+    const data = await response.json();
+    console.log(data);
+    $n.textContent = `${data.name}`;
+    $b.textContent = `${data.blog}`;
+  } catch (err) {
+    handleError(err);
+  }
 }
 
 function handleError(err) {
